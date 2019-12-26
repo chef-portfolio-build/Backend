8e7eaaa0794f0ff26c540637cbd28dbfa71bdb07
eralpkor <eralp.ny@gmail.com>
2019-12-25 20:49:55 -0500

    :bug: bug fixed with user validation middleware, i exp-validator :ambulance:

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,43 +1,46 @@
 const router = require('express').Router();
 require('dotenv').config();
 const secret = require('../config/secrets.js');
+const validateNewUser = require('./validNewUser');
+const HashFactor = parseInt(process.env.HASH) || 8;
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('./auth-model');
 
-// POST register new chef
-router.post('/register', (req, res) => {
-  const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 8);
-  user.password = hash;
 
-  Users.add(user)
-    .then(a => {
-      const token = generateToken(a);
-      res.status(201).json({ user: a, token });
+
+// POST register new chef
+router.post('/register', validateNewUser, (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, HashFactor);
+  user.password = hash;
+  
+  Users.addUser(user)
+    .then(newUser => {
+      console.log(newUser);
+      const token = generateToken(newUser);
+      res.status(201).json({ user: newUser, token});
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({message: err});
+      res.status(500).json(err);
     })
+  
 });
 
 // login user
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  Users.findBy({ username })
+  Users.findByName({ username })
     .first()
     .then(u => {
       if (u && bcrypt.compareSync(password, u.password)) {
         const token = generateToken(u);
 
-        res.status(200).json({
-          user: u.username,
-          token,
-        });
+        res.status(200).json({ message: `Welcome back ${u.username }`, user: u.username,token});
       } else {
         res.status(401).json({ message: `Wrong login credentials.`})
       }
